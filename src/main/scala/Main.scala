3553d3cce1e853616bd1c904a311ac69dfb9c7a5
niket01 <niket01tv@gmail.com>
2020-03-11 23:45:22 +0400

    add entity Crash

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,11 +10,23 @@ import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.pubsub.{PubsubUtils, SparkGCPCredentials}
 
 object Main {
+  case class Crash(CrashDate: String,
+                   CrashTime: String,
+                   BOROUGH: String,
+                   ZIP_CODE: String,
+                   PERSONS_INJURED: Int,
+                   PERSONS_KILLED: Int,
+                   PEDESTRIANS_INJURED: Int,
+                   PEDESTRIANS_KILLED: Int,
+                   CYCLIST_INJURED: Int,
+                   CYCLIST_KILLED: Int,
+                   MOTORIST_INJURED: Int,
+                   MOTORIST_KILLED: Int
+                  )
+
   def main(args: Array[String]) : Unit = {
-    val totalRunningTime = "1"
     val projectID = "igneous-equinox-269508"
-    val windowLength: Int = 20
-    val slidingInterval: Int = 60
+    val slidingInterval: Int = 60*60
 
     val conf = new SparkConf().setMaster("local[2]").setAppName("SparkCrashes")
     val ssc = new StreamingContext(conf, Seconds(slidingInterval))
@@ -24,7 +36,7 @@ object Main {
         ssc,
         projectID,
         None,
-        "crashes_subscription",
+        "crashes_subscriptions",
         SparkGCPCredentials.builder.build(), StorageLevel.MEMORY_AND_DISK_SER_2)
       .map(message => new String(message.getData(), StandardCharsets.UTF_8))
 
@@ -33,12 +45,29 @@ object Main {
         val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
         import spark.implicits._
 
-        val format = "yyyyMMdd_HHmmss"
+        val format = "yyyyMMdd"
         val dtf = DateTimeFormatter.ofPattern(format)
         val ldt = LocalDateTime.now()
 
-        val wordsDataFrame = rdd.toDF("word")
-        wordsDataFrame.write.parquet("gs://crashes_bucket/data/" + ldt.format(dtf))
+        val split = rdd.map(_.split(","))
+        val crash = split.map(r =>
+          Crash(
+            r(0),
+            r(1),
+            r(2),
+            r(3),
+            r(10).toInt,
+            r(11).toInt,
+            r(12).toInt,
+            r(13).toInt,
+            r(14).toInt,
+            r(15).toInt,
+            r(16).toInt,
+            r(17).toInt
+          )
+        )
+
+        crash.toDF().write.parquet("gs://crashes_bucket/data/" + ldt.format(dtf))
     }
 
     ssc.start()             // Start the computation
