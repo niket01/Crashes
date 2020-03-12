3727191abc0fd97ca188019db9f333fe9076c47c
niket01 <niket01tv@gmail.com>
2020-03-12 22:23:23 +0400

    without tempView

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,15 @@
 import java.nio.charset.StandardCharsets
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.Window.orderBy
+import org.apache.spark.sql.functions._
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.pubsub.{PubsubUtils, SparkGCPCredentials}
 
 object Main {
-
   def main(args: Array[String]) : Unit = {
     val projectID = "igneous-equinox-269508"
     val slidingInterval: Int = 5*60
@@ -24,6 +25,8 @@ object Main {
       .config(conf)
       .getOrCreate()
 
+    import spark.implicits._
+
     val messagesStream: DStream[String] = PubsubUtils
       .createStream(
         ssc,
@@ -35,46 +38,70 @@ object Main {
 
     messagesStream.foreachRDD{
       rdd =>
-        val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
-        import spark.implicits._
 
-        val crash = rdd.toDF("CRASH_DATE,CRASH_TIME,BOROUGH,ZIP_CODE,LATITUDE,LONGITUDE,LOCATION," +
-          "ON_STREET_NAME,CROSS_STREET_NAME,OFF_STREET_NAME,NUMBER_OF_PERSONS_INJURED,NUMBER_OF_PERSONS_KILLED," +
-          "NUMBER_OF_PEDESTRIANS_INJURED,NUMBER_OF_PEDESTRIANS_KILLED,NUMBER_OF_CYCLIST_INJURED," +
-          "NUMBER_OF_CYCLIST_KILLED,NUMBER_OF_MOTORIST_INJURED,NUMBER_OF_MOTORIST_KILLED," +
-          "CONTRIBUTING_FACTOR_VEHICLE_1,CONTRIBUTING_FACTOR_VEHICLE_2,CONTRIBUTING_FACTOR_VEHICLE_3," +
-          "CONTRIBUTING_FACTOR_VEHICLE_4,CONTRIBUTING_FACTOR_VEHICLE_5,COLLISION_ID,VEHICLE_TYPE_CODE_1," +
-          "VEHICLE_TYPE_CODE_2,VEHICLE_TYPE_CODE_3,VEHICLE_TYPE_CODE_4,VEHICLE_TYPE_CODE_5")
+        val crashDF = rdd.toDF("CRASH_DATE",
+          "CRASH_TIME",
+          "BOROUGH",
+          "ZIP_CODE",
+          "LATITUDE",
+          "LONGITUDE",
+          "LOCATION",
+          "ON_STREET_NAME",
+          "CROSS_STREET_NAME",
+          "OFF_STREET_NAME",
+          "NUMBER_OF_PERSONS_INJURED",
+          "NUMBER_OF_PERSONS_KILLED",
+          "NUMBER_OF_PEDESTRIANS_INJURED",
+          "NUMBER_OF_PEDESTRIANS_KILLED",
+          "NUMBER_OF_CYCLIST_INJURED",
+          "NUMBER_OF_CYCLIST_KILLED",
+          "NUMBER_OF_MOTORIST_INJURED",
+          "NUMBER_OF_MOTORIST_KILLED",
+          "CONTRIBUTING_FACTOR_VEHICLE_1",
+          "CONTRIBUTING_FACTOR_VEHICLE_2",
+          "CONTRIBUTING_FACTOR_VEHICLE_3",
+          "CONTRIBUTING_FACTOR_VEHICLE_4",
+          "CONTRIBUTING_FACTOR_VEHICLE_5",
+          "COLLISION_ID,VEHICLE_TYPE_CODE_1",
+          "VEHICLE_TYPE_CODE_2",
+          "VEHICLE_TYPE_CODE_3",
+          "VEHICLE_TYPE_CODE_4",
+          "VEHICLE_TYPE_CODE_5"
+        )
 
-        val view = crash.createTempView("Crashes")
+        val newCrashDF = crashDF.withColumn("timestamp", current_timestamp())
 
-        val sqlDF = spark.sql("select *, current_timestamp() as timestamp from view")
+        val top20_ZIP_CODE =
+        newCrashDF
+          .where("ZIP_CODE != ''")
+          .groupBy("ZIP_CODE")
+          .agg(
+            count("*").alias("count_crashes"),
+            first("timestamp").alias("timestamp"),
+            row_number().over(orderBy(count(("*")).desc)).alias("rank")
+          )
+          .select("rank", "ZIP_CODE", "count_crashes", "timestamp")
+          .limit(20)
 
-        val top20_ZIP_CODE = spark.sql("""
-                                  select ZIP_CODE,
-                                         count(*),
-                                         rownum() partition by (ZIP_CODE order by count(*) desc) as rnk,
-                                         first(timestamp)
-                                    from Crashes
-                                   where ZIP_CODE != ''
-                                   group ZIP_CODE
-                                   limit 20""")
-
-        val top10_BOROUGH = spark.sql("""
-                                  select BOROUGH,
-                                         count(*),
-                                         rownum() partition by (BOROUGH order by count(*) desc) as rnk,
-                                         first(timestamp)
-                                    from Crashes
-                                   where BOROUGH != ''
-                                   group BOROUGH
-                                   limit 10""")
+        val top10_BOROUGH =
+          newCrashDF
+            .where("BOROUGH != ''")
+            .groupBy("BOROUGH")
+            .agg(
+              count("*").alias("count_crashes"),
+              first("timestamp").alias("timestamp"),
+              row_number().over(orderBy(count(("*")).desc)).alias("rank")
+            )
+            .select("rank", "BOROUGH", "count_crashes", "timestamp")
+            .limit(10)
 
         /* здесь должны быть пострадавшие пассажиры */
 
         /* коннектор к big query + импорт данных в созданные таблицы */
 
-        sqlDF.write.parquet("gs://crashes_bucket/data/")
+        newCrashDF.write.parquet("gs://crashes_bucket/data/" + current_timestamp().toString())
+        top20_ZIP_CODE.write.parquet("gs://crashes_bucket/top20_ZIP_CODE/" + current_timestamp().toString())
+        top10_BOROUGH.write.parquet("gs://crashes_bucket/top10_BOROUGH/" + current_timestamp().toString())
     }
 
     ssc.start()             // Start the computation
